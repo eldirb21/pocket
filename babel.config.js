0aeebf3036291409c1894667331fecef50b6aa26
eldirb21 <eldirb95@gmail.com>
2024-08-19 07:09:01 +0700

    init pocket

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,44 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          // components
+          '@atoms': './src/components/atoms',
+          '@molecules': './src/components/molecules',
+
+          // assets
+          '@icons': './src/assets/icons',
+          '@images': './src/assets/images',
+
+          // constants
+          '@constants': './src/constants',
+
+          // helpers
+          '@helpers': './src/helpers',
+
+          '@hooks': ['./src/hooks'],
+
+          // navigate
+          '@navigate': './src/navigate',
+
+          // pages
+          '@pages': './src/pages',
+
+          // services
+          '@services': './src/services',
+
+          // utils
+          '@utils': './src/utils',
+
+          // stores
+          '@stores': './src/stores',
+        },
+      },
+    ],
+  ],
 };
